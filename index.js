6a5d2e5d5ac05ae67d0b7c94d7a7a738055e3e7b
Daxomar <152349184+Daxomar@users.noreply.github.com>
2024-03-31 04:52:50 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function Book(title,author,pages,read){
+    this.title = title;
+    this.author=author;
+    this.pages= pages;
+    this.read = read; 
+
+    this.report = () => {
+        console.log(this.title + " by " + this.author +"," + this.pages + ", " + this.read)
+    }
+
+}
+
+const Book1 = new Book("The fisherman", "David Chukwurah" , 222, "Not read");
+
+Book1.report();
